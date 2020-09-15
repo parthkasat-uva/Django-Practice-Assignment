f1cc6bf36a15b15b6fdcb1c836e9537b87591c53
ParthKasat <parthkasat@gmail.com>
2020-09-14 21:36:09 -0400

    Update urls.py

--- polls/urls.py
+++ polls/urls.py
@@ -5,17 +5,10 @@ app_name = 'polls'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('comments',views.CommentsView.as_view(), name='comments'),
+    path('comments/list/', views.CommentsListView.as_view(), name='list'),
+    path('comments/add/', views.add, name='add'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
-    # # ex: /polls/
-    # path('', views.index, name='index'),
-    # # ex: /polls/5/
-    # path('<int:question_id>/', views.detail, name='detail'),
-    # # ex: /polls/5/results/
-    # path('<int:question_id>/results/', views.results, name='results'),
-    # # ex: /polls/5/vote/
-    # path('<int:question_id>/vote/', views.vote, name='vote'),
-    # # added the word 'specifics'
-    # path('specifics/<int:question_id>/', views.detail, name='detail'),
 ]
